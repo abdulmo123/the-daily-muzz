5dbaf7d7ca5b28f49453e47190c0f464f2815b14
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2025-08-13 17:56:52 -0500

    immediate rss feed fetch on start

--- backend/scheduler.js
+++ backend/scheduler.js
@@ -1,6 +1,18 @@
 import cron from 'node-cron';
-import { fetchAllFeeds } from './scraping/fetchRss';
+import { fetchAllFeeds } from './scraping/fetchRss.js';
 
+// run on startup
+(async () => {
+    console.log('Running initial RSS fetch on startup ...');
+    try {
+        await fetchAllFeeds();
+        console.log('Initial RSS fetch completed!');
+    } catch (err) {
+        console.error('Error during initial RSS fetch: ', err);
+    }
+})();
+
+// scheduled 7 AM cron job
 cron.schedule('0 7 * * *', async () => {
     console.log('Running daily RSS fetch job at 7AM');
     try {
