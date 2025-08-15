ad3242615fb7d1ab378dce6c9a1be697142a2f1e
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2025-08-15 18:46:31 -0500

    endpoint for add subscribers to newsletter

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { fetchArticles } = require('./scraping/fetchRss');
+const { addSubscriber } = require('./email/subscriber');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.get('/articles', async (req, res) => {
@@ -10,6 +14,25 @@ app.get('/articles', async (req, res) => {
         res.json(articles);
     } catch (err) {
         res.status(500).json({error: err.message});
+    }
+});
+
+app.post('/subscribers', async (req, res) => {
+    try {
+        const { email } = req.body;
+        console.log('Received data:', { email });
+
+        const subscriber = await addSubscriber({email});
+
+        res.status(201).json({
+            message: 'Subscriber added successfully!',
+            subscriber
+        });
+    } catch (error) {
+        console.error('Error adding subscriber:', error);
+        res.status(500).json({
+            message: 'Failed to add subscriber'
+        });
     }
 });
 
