3a09987b24880d69208aa4e72498ddc143f66e23
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2025-08-14 20:02:54 -0500

    start UI for email subscription to newsletter ... incomplete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,38 @@
 import { useState } from 'react'
+import TextField from '@mui/material/TextField';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, FormControl, FormLabel } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState("");
+  
+  const handleChange = (e: any) => {
+    setEmail(e.target.value);
+  }
+
+  const handleAddSubscriber = async () => {
+    try {
+      const response = await fetch('/subscribe', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email}),
+      });
+
+      if (response.ok) {
+        console.log('Subscribed successfully!');
+        setEmail('');
+      } else {
+        console.error('Unable to subscribe :(');
+      }
+    }
+    catch (error) {
+      console.error('Unable to subscribe :(');
+    }
+  }
 
   return (
     <>
@@ -18,9 +46,18 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <FormControl>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <TextField
+            label="Email"
+            value={email}
+            onChange={handleChange}
+            placeholder="Enter your email"
+          />
+        </FormControl> 
+        <Button type="submit" fullWidth variant="contained" onClick={handleAddSubscriber}>
+          Sign in
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
