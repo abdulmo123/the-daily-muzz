d732ac8f9ab21666df7ac38f1db0458c573b3fd1
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2025-08-19 18:22:47 -0500

    create db client (for local vs supabase)

---
+++ server/db/client.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
+
+let client;
+
+if (process.env.DB_CLIENT === 'pg') {
+  client = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT || 5432,
+  });
+} else if (process.env.DB_CLIENT === 'supabase') {
+  client = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+}
+
+module.exports = client;
