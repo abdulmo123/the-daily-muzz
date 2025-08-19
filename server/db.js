2ffa9005e32f83f905424435ff8c54038137bf40
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2025-08-19 18:49:39 -0500

    attempt to fix github action fails

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-const createClient = require('@supabase/supabase-js');
+const { createClient } = require('@supabase/supabase-js');
 dotenv.config();
 
 const { Pool } = require('pg');
