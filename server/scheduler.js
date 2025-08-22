1ab898e0c8315dda96cd52e580fb480917cd7c4b
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2025-08-21 19:46:40 -0500

    move process to scheduler + delete run.js

--- server/scheduler.js
+++ server/scheduler.js
@@ -2,7 +2,6 @@ const cron = require('node-cron');
 const { fetchAllFeeds } = require('./scraping/fetchRss');
 const { sendNewsletter } = require('./email/sendNewsletter');
 
-// run on startup
 (async () => {
     console.log('Running initial RSS fetch on startup ...');
     try {
@@ -11,25 +10,10 @@ const { sendNewsletter } = require('./email/sendNewsletter');
 
         await sendNewsletter();
         console.log('Newsletter sent successfully!');
+
+        process.exit(0);
     } catch (err) {
         console.error('Error during initial RSS fetch: ', err);
+        process.exit(1);
     }
 })();
-
-// scheduled 7 AM cron job
-cron.schedule('0 7 * * *', async () => {
-    console.log('Running daily RSS fetch job at 7AM');
-    try {
-        await fetchAllFeeds();
-        console.log("RSS fetch completed successfully!");
-
-        await sendNewsletter();
-        console.log('Newsletter sent successfully!');
-        
-    }
-    catch (err) {
-        console.error("Error running RSS fetch: ", err);
-    }
-});
-
-console.log("Scheduler started, waiting for scheduled jobs ...");
