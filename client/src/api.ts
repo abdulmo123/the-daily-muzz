f17a3d32918b39f00d844229545ab070aa0c322d
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2025-09-04 16:53:51 -0500

    add api call for unsubscribe

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,15 +1,30 @@
 const BACKEND_URI = "http://localhost:3000";
 
 export async function addSubscriber(email: string) {
-    const response = await fetch(`${BACKEND_URI}/subscribers`, {
+    const response = await fetch(`${BACKEND_URI}/subscribe`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
       });
 
       if (!response.ok) {
-        throw new Error(`Deleting file failed: ${response.statusText}`);
+        throw new Error(`Adding subscriber failed: ${response.statusText}`);
     }
     const data = await response.json();
     return data;
+}
+
+export async function removeSubscriber(email: string) {
+  const response = await fetch(`${BACKEND_URI}/unsubscribe`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Removing subscriber failed: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return data;
 }
